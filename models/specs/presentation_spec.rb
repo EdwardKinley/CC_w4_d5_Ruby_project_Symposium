800e5c87d913b9e077bb764b5a3e46556bbdb274
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-16 00:12:07 +0100

    tests presentation_spec.rb setup

---
+++ models/specs/presentation_spec.rb
@@ -0,0 +1,19 @@
+require("minitest/autorun")
+require_relative("../presentation.rb")
+
+class TestPresentation < MiniTest::Test
+
+  def setup()
+    options = {"id" => 1, "name" => "Io", "time_slot" => 3, "capacity" => 12}
+    @presentation1 = Presentation.new(options)
+  end
+
+  def test_setup()
+    assert_equal("Io", @presentation1.name)
+    assert_equal(3, @presentation1.time_slot)
+    assert_equal(12, @presentation1.capacity)
+  end
+
+
+
+end
