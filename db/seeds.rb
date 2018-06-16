b9bd36a597aa0258e4b8199dea98587bdceaac2f
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-16 23:44:49 +0100

    sets up seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,57 @@
+require_relative("../models/delegate.rb")
+require_relative("../models/presentation.rb")
+require_relative("../models/registration.rb")
+require("pry-byebug")
+
+# Registration.delete_all()
+# Presentation.delete_all()
+# Delegate.delete_all()
+
+delegate1 = Delegate.new({"name" => "Colby Galileo", "board_member" => true})
+
+delegate2 = Delegate.new({"name" => "Thomas Feser"})
+
+delegate3 = Delegate.new({"name" => "Serena Davids", "board_member" => true})
+
+delegate4 = Delegate.new({"name" => "Neil Oderberg"})
+
+delegate5 = Delegate.new({"name" => "Ellen Ockham"})
+
+# delegate1.save()
+# delegate2.save()
+# delegate3.save()
+# delegate4.save()
+# delegate5.save()
+
+presentation1 = Presentation.new({"name" => "Io, Io, it's Off to Space We Go", "time_slot" => 1, "capacity" => 50})
+
+presentation2 = Presentation.new({"name" => "Europa League", "time_slot" => 2, "capacity" => 40})
+
+presentation3 = Presentation.new({"name" => "Gonnae Meet Ganymede", "time_slot" => 3, "capacity" => 10})
+
+presentation4 = Presentation.new({"name" => "Callisto Calypso", "time_slot" => 3, "capacity" => 4})
+
+presentation5 = Presentation.new({"name" => "Meeting Metis", "time_slot" => 4, "capacity" => 50})
+
+presentation6 = Presentation.new({"name" => "Adrastea and Amalthea", "time_slot" => 5, "capacity" => 10})
+
+presentation7 = Presentation.new({"name" => "Thebe Themes", "time_slot" => 5, "capacity" => 5})
+
+# presentation1.save()
+# presentation2.save()
+# presentation3.save()
+# presentation4.save()
+# presentation5.save()
+# presentation6.save()
+# presentation7.save()
+
+# delegate1.register(presentation1)
+# delegate1.register(presentation2)
+# delegate1.register(presentation3)
+# delegate1.register(presentation4)
+# delegate1.register(presentation7)
+# delegate2.register(presentation5)
+# delegate3.register(presentation6)
+# delegate3.register(presentation7)
+# delegate4.register(presentation2)
+# delegate5.register(presentation3)
--- models/specs/delegate_spec.rb
+++ models/specs/delegate_spec.rb
@@ -4,15 +4,18 @@ require_relative("../delegate.rb")
 class TestDelegate < MiniTest::Test
 
   def setup()
-    options = {"id" => 1, "name" => "Colby Galileo", "board_member" => true}
-    @delegate1 = Delegate.new(options)
+    options1 = {"id" => 1, "name" => "Colby Galileo", "board_member" => true}
+    @delegate1 = Delegate.new(options1)
+    options2 = {"id" => 2, "name" => "Thomas Feser"}
+    @delegate2 = Delegate.new(options2)
   end
 
   def test_setup()
     assert_equal("Colby Galileo", @delegate1.name)
     assert(@delegate1.board_member)
+    assert(!@delegate2.board_member)
   end
 
-  
+
 
 end
