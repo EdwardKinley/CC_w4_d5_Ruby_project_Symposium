c68a087135cca3d9b0dd623946f6a7d7a16a0aa9
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-18 16:48:56 +0100

    sets up index.erb and show.erb in views/registrations

--- controllers/registrations_controller.rb
+++ controllers/registrations_controller.rb
@@ -2,3 +2,13 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/registration.rb')
 also_reload('../models/*')
+
+get '/registrations' do
+  @registrations = Registration.all()
+  erb(:"registrations/index")
+end
+
+get '/registrations/:id' do
+  @registration = Registration.find(params['id'].to_i())
+  erb(:"registrations/show")
+end
