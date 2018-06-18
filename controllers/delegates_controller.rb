b1d5c5f3f232ee0c64179c51082019f467302a45
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-18 16:35:38 +0100

    sets up index.erb and show.erb in views/delegates

--- controllers/delegates_controller.rb
+++ controllers/delegates_controller.rb
@@ -2,3 +2,13 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/delegate.rb')
 also_reload('../models/*')
+
+get '/delegates' do
+  @delegates = Delegate.all()
+  erb(:"delegates/index")
+end
+
+get '/delegates/:id' do
+  @delegate = Delegate.find(params['id'].to_i())
+  erb(:"delegates/show")
+end
