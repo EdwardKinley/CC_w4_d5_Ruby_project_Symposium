3d8cb20a1df45a9c3b5d94ba188b199dfcc0b713
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-18 22:10:43 +0100

    adds views/presentations/new.erb

--- controllers/presentations_controller.rb
+++ controllers/presentations_controller.rb
@@ -3,12 +3,20 @@ require('sinatra/contrib/all')
 require_relative('../models/presentation.rb')
 also_reload('../models/*')
 
-get '/presentations' do
+get '/presentations' do #index
   @presentations = Presentation.all()
   erb(:"presentations/index")
 end
 
-get '/presentations/:id' do
+get '/presentations/new' do #new
+  erb(:"presentations/new")
+end
+
+get '/presentations/:id' do #show
   @presentation = Presentation.find(params['id'].to_i())
   erb(:"presentations/show")
 end
+
+# post '/presentations' do #create
+#
+# end
