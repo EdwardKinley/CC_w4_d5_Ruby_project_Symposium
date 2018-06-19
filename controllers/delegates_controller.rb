4dc5d11c0a7e503609a3d140f71db2d92545756a
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-19 10:00:36 +0100

    adds views/delegates/new.erb and /create.erb

--- controllers/delegates_controller.rb
+++ controllers/delegates_controller.rb
@@ -3,12 +3,22 @@ require('sinatra/contrib/all')
 require_relative('../models/delegate.rb')
 also_reload('../models/*')
 
-get '/delegates' do
+get '/delegates' do #index
   @delegates = Delegate.all()
   erb(:"delegates/index")
 end
 
-get '/delegates/:id' do
+get '/delegates/new' do #new
+  erb(:"delegates/new")
+end
+
+get '/delegates/:id' do #show
   @delegate = Delegate.find(params['id'].to_i())
   erb(:"delegates/show")
+end
+
+post '/delegates' do #create
+  @delegate = Delegate.new(params)
+  @delegate.save()
+  erb(:"delegates/create")
 end
