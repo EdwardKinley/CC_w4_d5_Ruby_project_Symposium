b04a6f8560ff3eb0e7d6b2d9732b373ab123c16b
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-19 10:18:55 +0100

    adds views/registrations/new.erb and /create.erb

--- controllers/registrations_controller.rb
+++ controllers/registrations_controller.rb
@@ -3,12 +3,24 @@ require('sinatra/contrib/all')
 require_relative('../models/registration.rb')
 also_reload('../models/*')
 
-get '/registrations' do
+get '/registrations' do #index
   @registrations = Registration.all()
   erb(:"registrations/index")
 end
 
-get '/registrations/:id' do
+get '/registrations/new' do #new
+  @presentations = Presentation.all
+  @delegates = Delegate.all
+  erb(:"registrations/new")
+end
+
+get '/registrations/:id' do #show
   @registration = Registration.find(params['id'].to_i())
   erb(:"registrations/show")
+end
+
+post '/registrations' do #create
+  @registration = Registration.new(params)
+  @registration.save()
+  erb(:"registrations/create")
 end
